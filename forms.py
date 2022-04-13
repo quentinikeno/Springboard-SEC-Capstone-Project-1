72266192ad3b32039c9e3812cee36a23b0407236
Quentin Ikeno <ikenoq@gmail.com>
2022-04-12 21:14:43 -1000

    Create form to create new worksheet

---
+++ forms.py
@@ -0,0 +1,15 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, SelectField, IntegerRangeField
+from wtforms.validators import DataRequired, Email, Length, NumberRange
+
+class CreateWorksheetForm(FlaskForm):
+    """Form for creating a new Worksheet."""
+
+    name = TextAreaField('Name', validators=[DataRequired()])
+    operations = SelectField('Type of Operations', choices=[
+        ('Addition', 'Addition'), 
+        ('Subtraction', 'Subtraction'), 
+        ('Multiplication', 'Multiplication'), 
+        ('Division', 'Division'), 
+        ('Random', 'Random')])
+    numberQuestions = IntegerRangeField('Number of Questions', validators=[NumberRange(min=5, max=20)])
