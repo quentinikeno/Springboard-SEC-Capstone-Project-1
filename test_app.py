7c9603b03c626faa2ca6c250105841ff2d1edd05
Quentin Ikeno <ikenoq@gmail.com>
2022-04-16 17:16:12 -1000

    Add tests for answer key routes

--- test_app.py
+++ test_app.py
@@ -37,6 +37,15 @@ class WorksheetViewsTestCase(TestCase):
             with client.session_transaction() as session:
                 self.assertIsNotNone(session["questions"])
                 
+    def test_new_worksheet_route_no_questions_in_session(self):
+        """Testing redirect from new worksheet route if questions are not in session."""
+        with app.test_client() as client:
+            resp = client.get('/worksheet/new', follow_redirects=True)
+            html = resp.get_data(as_text=True)
+            
+            self.assertEqual(resp.status_code, 200)
+            self.assertIn('Please generate a new worksheet before accessing that page.', html)
+                
     def test_worksheet_pdf_route(self):
         """Testing rendering of worksheet pdf."""
         with app.test_client() as client:
@@ -53,9 +62,33 @@ class WorksheetViewsTestCase(TestCase):
             self.assertEqual(resp.mimetype, 'application/pdf')
             
     def test_worksheet_pdf_route_no_questions_in_session(self):
-        """Testing rendering of worksheet pdf."""
+        """Testing redirect from pdf route if questions are not in session."""
         with app.test_client() as client:
             resp = client.get('/worksheet/new/pdf', follow_redirects=True)
+            html = resp.get_data(as_text=True)
+            
+            self.assertEqual(resp.status_code, 200)
+            self.assertIn('Please generate a new worksheet before accessing that page.', html)
+            
+    def test_answer_key_pdf_route(self):
+        """Testing rendering of answer key pdf."""
+        with app.test_client() as client:
+            with client.session_transaction() as session:
+                # Add questions to session
+                session['questions'] = [{'answer': 44, 'expression': '88 / 2', 'first': 88, 'operation': '/', 'second': 2}, 
+                                        {'answer': 1440, 'expression': '40 * 36', 'first': 40, 'operation': '*', 'second': 36}, 
+                                        {'answer': 30, 'expression': '45 - 15', 'first': 45, 'operation': '-', 'second': 15},
+                                        {'answer': 12, 'expression': '40 - 28', 'first': 40, 'operation': '-', 'second': 28}, 
+                                        {'answer': 15, 'expression': '56 - 41', 'first': 56, 'operation': '-', 'second': 41}]
+            resp = client.get('/answer-key/new/pdf')
+            
+            self.assertEqual(resp.status_code, 200)
+            self.assertEqual(resp.mimetype, 'application/pdf')
+            
+    def test_answer_key_pdf_route_no_questions_in_session(self):
+        """Testing redirect from answer key pdf route if questions are not in session."""
+        with app.test_client() as client:
+            resp = client.get('/answer-key8/new/pdf', follow_redirects=True)
             html = resp.get_data(as_text=True)
             
             self.assertEqual(resp.status_code, 200)
