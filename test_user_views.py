58f6184be95d2402bf14eb1e8b21a45a081f0b0c
Quentin Ikeno <ikenoq@gmail.com>
2022-04-16 23:13:04 -1000

    Create tests for users login

---
+++ test_user_views.py
@@ -0,0 +1,55 @@
+from app import app
+from unittest import TestCase
+from models import db, User
+from sqlalchemy.exc import IntegrityError
+
+app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql:///worksheet_generator_test"
+app.config['SQLALCHEMY_ECHO'] = False
+app.config['TESTING'] = True
+app.config['DEBUG_TB_HOSTS'] = ['dont-show-debug-toolbar']
+app.config['WTF_CSRF_ENABLED'] = False
+
+db.drop_all()
+db.create_all()
+
+class UserViewsTestCase(TestCase):
+    """Test User Views."""
+
+    def setUp(self):
+        """Create test client, add sample data."""
+        
+        User.query.delete()
+        
+        user = User.register(
+            "JaneDoe",
+            "GreatPassword123",
+            "test@email.com"
+        )
+        
+        db.session.add(user)
+        db.session.commit()
+        
+        self.user = user
+        
+    def tearDown(self):
+        """Rollback any failed transactions."""
+        db.session.rollback()
+        
+    def test_register_get(self):
+        with app.test_client() as client:
+            resp = client.get('/register')
+            html = resp.get_data(as_text=True)
+            
+            self.assertEqual(resp.status_code, 200)
+            self.assertIn('<h1>Register Your New Account</h1>', html)
+            
+    def test_register_post(self):
+        with app.test_client() as client:
+            data = {'username': 'test', 'password': 'testPassword', 'email': 'test2@email.com'}
+            resp = client.post('/register', data=data, follow_redirects=True)
+            html = resp.get_data(as_text=True)
+            
+            self.assertEqual(resp.status_code, 200)
+            self.assertIn('<h1>Your Profile</h1>', html)
+            with client.session_transaction() as session:
+                self.assertIsNotNone(session['user'])
