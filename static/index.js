938684a1932df405f39d2b77f73d46c14ce036bf
Quentin Ikeno <ikenoq@gmail.com>
2022-04-18 22:33:34 -1000

    Change name of app.js to index.js and add loading spinner for submit button on form submission.

---
+++ static/index.js
@@ -0,0 +1,23 @@
+const range_input = $("#number_questions");
+
+function updateRangeVal() {
+	$("#range-val").text(range_input.val());
+}
+
+// Show value of range slider.
+$(document).ready(function () {
+	updateRangeVal();
+	range_input.on("change", updateRangeVal);
+});
+
+// On submit, disable the form submission button and show a loading spinner.
+$("form").on("submit", function () {
+	buttonLoading = $(
+		'<span class="spinner-border spinner-border-sm" role="status" aria-hidden="true"></span>'
+	);
+	submitButton = $("form button");
+	submitButton
+		.text("Loading...  ")
+		.append(buttonLoading)
+		.prop("disabled", true);
+});
