29fa366b19fea50356bb4849276da5c2a1d8088b
Quentin Ikeno <ikenoq@gmail.com>
2022-04-24 13:05:02 -1000

    Update seed file.

--- seed.py
+++ seed.py
@@ -1,14 +1,5 @@
-from models import db, User, PDF
+from models import db
 from app import app
 
 db.drop_all()
 db.create_all()
-
-user = User.register(
-            "test",
-            "test",
-            "test@email.com"
-        )
-        
-db.session.add(user)
-db.session.commit()
